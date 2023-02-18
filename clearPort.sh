db4abeac958d49bcfd1d53d3b94b6c594ff28f54
Collins Kareri <60278794+Collins-Kareri@users.noreply.github.com>
2023-02-18 07:17:28 +0300

    Refactor for better readability.

--- clearPort.sh
+++ clearPort.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 number_regex='^[0-9]+$'
 
-echo Enter the port to close:
-while read port; do
-  if ! [[ $port =~ $number_regex ]] ; then
-    continue
-  fi
-  sudo kill $(sudo lsof -t -i:$port)
-  break
-done
+read -rp "Enter the port to close: " port
 
-echo Port now available
+if [[ -z "$port" || ! "$port" =~ $number_regex ]]
+then
+	echo "You did not enter a valid port value"
+	exit
+fi
+
+pid=$(sudo lsof -t -i:"$port")
+
+if [[ -z "$pid" ]]
+then
+	echo "Port number is already open. Nothing to do here."
+	exit
+fi
+
+echo -e "\n\bThis script uses sudo permission to close port\n\b"
+
+sudo kill $pid
+
+echo "Killing port..."
+echo "Port is now available"
